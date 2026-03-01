faf11c579aa676e002e9014e2e6b565e3166e24b
Rami <Rami@DESKTOP-S7B1U8V>
2026-03-01 07:25:02 +0300

    Rename Password to HashedPassword, require Teacher.UserId
    Renamed Users.Password to HashedPassword for clarity and security. Made Teachers.UserId non-nullable with a default value, enforcing user association. Updated model snapshot, navigation property, and seed data to reflect these changes. Added migration for schema update.

---
+++ AlAshmar.Infrastructure.Persistence/Migrations/20260227124611_UpdatePasswordColumnToHashedPassword.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AlAshmar.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatePasswordColumnToHashedPassword : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Password",
+                table: "Users",
+                newName: "HashedPassword");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "UserId",
+                table: "Teachers",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "HashedPassword",
+                table: "Users",
+                newName: "Password");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "UserId",
+                table: "Teachers",
+                type: "uniqueidentifier",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+        }
+    }
+}
